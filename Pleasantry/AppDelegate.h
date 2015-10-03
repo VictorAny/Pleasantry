1944173f6a44a659d1504e626385ce55a4b03384
Victor Anyirah <victoranyirah1@Victors-MBP.home>
2015-10-03 15:17:23 -0500

    Initial Commit

---
+++ Pleasantry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pleasantry
+//
+//  Created by Victor Anyirah on 10/3/15.
+//  Copyright (c) 2015 Victor Anyirah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
